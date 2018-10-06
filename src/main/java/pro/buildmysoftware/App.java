76ddca81ea9549b77cd2c2cbf0c3a864ed7d32ab
Mike Wojtyna <mike@buildmysoftware.pro>
2018-10-07 01:18:49 +0200

    Create standalone App using embedded tomcat

---
+++ src/main/java/pro/buildmysoftware/App.java
@@ -0,0 +1,15 @@
+package pro.buildmysoftware;
+
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		Tomcat tomcat = create();
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+
+	private static Tomcat create() throws Exception {
+		return EmbeddedTomcatFactory.create(8080);
+	}
+}
